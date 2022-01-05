2388ddf6ff4dd8cab89187b70000bf5481cc4ae0
lov3b <lovebillenius@protonmail.com>
2022-01-05 16:54:26 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,114 @@
 use std::env;
 use std::fs;
+use std::fs::ReadDir;
 use std::io;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 fn main() {
-    println!("Hello, world!");
-    let here = env::current_dir().unwrap();
+    let opt = Opt::from_args();
+    let resolution = opt.resolution;
+    let season_before_res = opt.season_before_res;
+    let dry_mode = opt.dry_mode;
+    //let resolution = "1080p".to_string();
+    //let season_before_res = true;
+    //let dry_mode = false;
 
-    let files = fs::read_dir(&here).unwrap();
+    let mut renamed_files: Vec<String> = Vec::new();
+    let mut rename_counter: usize = 0;
 
-    let mut entries = fs::read_dir(".")
-        .unwrap()
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()
-        .unwrap();
+    #[allow(unused_mut)]
+    let mut entries = fs::read_dir(".").unwrap();
 
-    // Entries here
-    for i in entries.iter() {
-        println!("{:?}", &i);
-    }
+    for i in entries.map(|e| e.unwrap()).into_iter() {
+        let current_name = i.file_name().into_string().unwrap();
 
-    for i in entries.iter() {
-        println!("{:?}", &i);
+        // If file does not match given resolution, keep on going
+        if !current_name.contains(&resolution) {
+            //println!("skip: {}", &current_name);
+            continue;
+        }
 
-        let tmp = i.to_str().unwrap().replace("./", "").replace(".", " ");
-        let resolution = if tmp.contains("1080p") {
-            "1080p"
-        } else if tmp.contains("720p") {
-            "720p"
-        } else if tmp.contains("2160p") {
-            "2160p"
-        } else if tmp.contains("2700p") {
-            "2700p"
-        } else if tmp.contains(".20") {
-            ".20"
-        } else if tmp.contains(".19") {
-            ".19"
+        println!("original filename: '{}'", &current_name);
+        let mut name_to_be = current_name.clone();
+        name_to_be = name_to_be.split_once(&resolution).unwrap().0.to_string();
+
+        // Remove last character, which will be a dot
+        let name_to_be: String = name_to_be.chars().take(&name_to_be.len() - 1).collect();
+
+        let dots = name_to_be.chars().filter(|x| x == &'.').count() - 1;
+        println!("dots: {}", &dots);
+
+        let mut counter: usize = 0;
+        let mut name_to_be: String = name_to_be
+            .chars()
+            .map(|x| {
+                if x == '.' {
+                    if counter == dots {
+                        return ':';
+                    }
+                    counter += 1;
+                }
+                x
+            })
+            .collect::<String>();
+
+        // If season is before resolution
+        if season_before_res {
+            name_to_be = name_to_be.replace(":", "꞉ "); // This is not a regular colon
         } else {
-            println!("Could not find resulotion: {}", &tmp);
-            std::process::exit(0);
-        };
+            name_to_be = name_to_be.replace(":", "꞉"); // This is not a regular colon
+        }
+        name_to_be = name_to_be.replace(".", " ");
+
+        println!("'{}'", &name_to_be);
+
+        let here = env::current_dir().unwrap();
+        let mut current = PathBuf::from(&here);
+        let mut to = PathBuf::from(&here);
+        current.push(&current_name);
+        to.push(&name_to_be.replace(" ", " "));
+
+        if !dry_mode {
+            match fs::rename(&current, &to) {
+                Ok(k) => k,
+                Err(e) => {
+                    println!("could not rename {}, \n{:?}", &current_name, &e)
+                }
+            };
+        }
+
+        rename_counter += 1;
+        renamed_files.push(name_to_be);
     }
+
+    // Print amount of changed files for satisfaction
+    if &rename_counter == &1 {
+        println!("renamed {}", renamed_files[0]);
+    } else {
+        println!("Renamed following files");
+        for i in renamed_files {
+            println!("{}", &i);
+        }
+        println!("which was {} files", &rename_counter);
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "episodeat")]
+struct Opt {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
+    /// Whats the resolution in the file-name
+    #[structopt(short, long)]
+    resolution: String,
+
+    /// Is season before_res
+    #[structopt(short, long)]
+    season_before_res: bool,
+
+    /// Do not rename anything
+    #[structopt(short, long)]
+    dry_mode: bool,
 }
